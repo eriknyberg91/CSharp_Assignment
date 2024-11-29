bcdf5797b150de79d504bc0e9dab35bfe9851f95
Erik <erikmikaelnyberg@gmail.com>
2024-11-29 11:12:38 +0100

    Add project files.

---
+++ MauiBlazor.MainApp/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MauiBlazor.MainApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ MauiBlazor.MainApp/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace MauiBlazor.MainApp
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
