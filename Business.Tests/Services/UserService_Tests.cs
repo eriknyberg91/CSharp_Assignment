d31a3c78f01cd868a68922f67d629f49e199c399
Erik <erikmikaelnyberg@gmail.com>
2024-12-09 14:54:57 +0100

    Added xUnit test for GetUsersFromList function

---
+++ Business.Tests/Services/UserService_Tests.cs
@@ -0,0 +1,21 @@
+﻿using Business.Services;
+using Business.Models;
+
+
+namespace Business.Tests.Services;
+public class UserServiceTests
+{
+    [Fact]
+    public void GetUsersFromList_ReturnsListOfUsers()
+    {
+        // Arrange
+        var userService = new UserService(new FileService());
+
+        // Act
+        var result = userService.GetUsersFromList();
+
+        // Assert
+        Assert.NotNull(result);
+        Assert.IsAssignableFrom<IEnumerable<User>>(result);
+    }
+}
